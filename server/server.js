23c14d5b233c31ee8577521230f677550bb1dec3
Ralph L <lettau.ralph@gmail.com>
2017-05-11 09:37:39 +0200

    git commit -m 'Add POST /todos route and refactor mongoose'

--- server/server.js
+++ server/server.js
@@ -1,64 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise; //mongoose with E6 Promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minLength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var {mongoose}  = require('./db/mongoose');
+var {Todo}      = require('./models/todo');
+var {User}      = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
 });
 
-// var newTodo = new Todo({
-//     text: 'Chuck Dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-var newTodo = new Todo({
-    text:'   Edit this video      '
-});
-
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc)
-}, (e) => {
-    console.log('Unable to save todo ',e)
-});
-
-//User
-//email - required trim -type string min length 1
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minLength: 1,
-        trim: true
-    }
-});
-
-var newUser = new User({
-    email: ' at@ at. at  '
+app.listen(3000, () => {
+    console.log('Started on Port 3000');
 })
 
-newUser.save().then((user) => {
-    console.log("Saved user");
-    console.log(user);
-}, (error) => {
-    console.log('Unable to save user ',error)
-})
 
